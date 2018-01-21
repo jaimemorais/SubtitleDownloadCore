746a5b648e6579ea5b56e064f80d395dbb2a03f5
jaimemorais <jaimemorais@gmail.com>
2018-01-21 12:52:37 -0200

    Change hash method

--- Program.cs
+++ Program.cs
@@ -74,6 +74,7 @@ namespace SubtitleDownloadCore
             using (HttpClient httpClient = new HttpClient())
             {
                 string subdbApiFileHash = GetSubdbFileHash(filePath);
+
                 string urlSearch = $"http://api.thesubdb.com/?action=search&hash={subdbApiFileHash}";
 
                 httpClient.DefaultRequestHeaders.Add("User-Agent", "SubDB/1.0 (SubtitleDownloadCore/1.0; http://github.com/jaimemorais/SubtitleDownloadCore)");
@@ -151,8 +152,44 @@ namespace SubtitleDownloadCore
                 throw;
             }
         }
-        
 
+
+
+
+
+        private static readonly object fsLock = new object();
+        
+        private static string GetSubdbFileHash(string filePath)
+        {
+            int bufferSize = 64 * 1024;
+            byte[] first64kb = new byte[bufferSize];
+            byte[] last64kb = new byte[bufferSize];
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open))
+            {
+                lock (fsLock)
+                {
+                    // first 64k
+                    fs.Seek(0, SeekOrigin.Begin);
+                    fs.Read(first64kb, 0, bufferSize);
+
+                    // last 64k
+                    fs.Seek(-bufferSize, SeekOrigin.End);                    
+                    fs.Read(last64kb, 0, bufferSize);
+                }
+            }
+
+            using (var md5 = MD5.Create())
+            {
+                byte[] concatBytes = first64kb.Concat(last64kb).ToArray();
+                var hash = md5.ComputeHash(concatBytes);
+                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();                
+            }
+            
+        }
+
+
+        /*
         public static byte[] StreamToByteArray(Stream input)
         {
             byte[] buffer = new byte[16 * 1024];
@@ -168,7 +205,7 @@ namespace SubtitleDownloadCore
         }
 
 
-        private static string GetSubdbFileHash(string filePath)
+        private static string GetHash(string filePath)
         {
             using (var md5 = MD5.Create())
             {
@@ -186,6 +223,6 @@ namespace SubtitleDownloadCore
                }
             }
         }
-
+        */
     }
 }
